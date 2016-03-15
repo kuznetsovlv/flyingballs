8c3e6a11a898dbce056dfed16422b4f476c81e65
Kuznetsov Leonid <kuznetsovlv1981@gmail.com>
2016-03-15 03:00:33 +0300

    algorithm.js: native HTML5 Drag & Drop changed to old

--- js/algorithm.js
+++ js/algorithm.js
@@ -3,16 +3,58 @@
 
 	var DIRECTION = 1;
 	var INTERVAL = 10;
+	var ZERO = 0.028;
+
+	try {
+		new CustomEvent("IE has CustomEvent, but doesn't support constructor");
+	} catch (e) {
+
+		window.CustomEvent = function(event, params) {
+			var evt;
+			params = params || {
+				bubbles: false,
+				cancelable: false,
+				detail: undefined
+			};
+
+			params = params || {};
+
+			if (!params.bubbles)
+				params.bubbles = false;
+			if (!params.cancelable)
+				params.cancelable = false;
+			evt = document.createEvent("CustomEvent");
+			evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+			return evt;
+		};
+
+		CustomEvent.prototype = Object.create(window.Event.prototype);
+	}
 
 	function on (target, type, handler) {
-		try {
-			target.addEventListener(type, handler, false);
-		} catch (e) {
-			target.attachEvent(['on', type].join(''), function (event) {
-				event = event || window.event;
-				return handler.call(target, event);
-			});
+		function h (event) {
+			event = event || window.event;
+			return handler.call(target, event);
 		}
+		try {
+			target.addEventListener(type, h, false);
+		} catch (e) {
+			target.attachEvent(['on', type].join(''), h);
+		}
+		return h;
+	}
+
+	function off (target, type, handler) {
+		try {
+			target.removeEventListener(type, handler, false);
+		} catch (e) {
+			target.detachEvent(['on', type].join(''), handler);
+		}
+	}
+
+	function inTarget (target, coords) {
+		var rect = target.getBoundingClientRect();
+		return coords.x > rect.left && coords.x < rect.right && coords.y > rect.top &&  coords.y < rect.bottom;
 	}
 
 	function Ball (elem) {
@@ -20,80 +62,76 @@
 
 		this.velocity = {x: 0, y: 0};
 
+		this.target = document.getElementsByTagName('main')[0];
+
 		var self = this;
 
-		on(elem, 'dragstart', function (event) {
-			var coords = this.getBoundingClientRect(),
-			    parentPos = this.parentNode.getBoundingClientRect();
+		function moving (event) {
+			this.style.left = [event.clientX - this.width / 2, 'px'].join('');
+			this.style.top = [event.clientY - this.height / 2, 'px'].join('');
 
-			self.x = coords.left - parentPos.left;
-			self.y = coords.top - parentPos.top;
-			self.now = Date.now();
-			self.cursor = {
-				x: event.clientX,
-				y: event.clientY
-			};
+				this.style.cursor = inTarget(self.target, {x: event.clientX, y: event.clientY}) ? 'move' : '';
 
-			self.velocities = [];
+			var dt = (Date.now() - self.now) / INTERVAL;
 
-			event.dataTransfer.setData("text", this.src);
-			event.dataTransfer.effectAllowed = 'move';
-
-			try {
-				event.dataTransfer.setDragImage(this, (coords.right - coords.left) / 2, (coords.bottom - coords.top) / 2);
-			} catch (e) {
-
+			if (dt) {
+				var dx = event.clientX - self.x,
+				    dy = event.clientY - self.y;
+				if (!self.velocities)
+					self.velocities = [{x: dx / dt, y: dy / dt}];
+				else
+					self.velocities.push({x: dx / dt, y: dy / dt});
+				self.x = event.clientX;
+				self.y = event.clientY;
+				while(self.velocities.length > 20)
+					self.velocities.shift();
 			}
+		}
 
-			self.store.dropped = self;
-		});
+		on(elem, 'mousedown', function (event) {
+			this.style.left = [event.clientX - this.width / 2, 'px'].join('');
+			this.style.top = [event.clientY - this.height / 2, 'px'].join('');
+			this.style.margin = 0;
+			this.style.position = 'fixed';
+			this.style.zIndex = 10000;
 
-		on(elem, 'drag' , function (event) {
-			var now = Date.now(),
-			    dt = (now - self.now) / INTERVAL;
+			self.velocity = {x: 0, y:0};
 
-			if (dt < 2)
-				return;
-			var dx = event.clientX - self.cursor.x,
-			    dy = event.clientY - self.cursor.y;
-			self.x += dx;
-			self.y += dy;
-			self.cursor = {
-				x: event.clientX,
-				y: event.clientY
-			};
-
-			
-			self.now = now;
-			self.velocities.push({
-				x: dx / dt,
-				y: dy / dt
+			self.handler = on(document, 'mousemove', function (event) {
+				moving.call(self.e, event);
 			});
-
-			self.velocity = {x: 0, y: 0};
-			var l = self.velocities.length;
-
-			if (l > 1) {
-				var i = 2,
-				    v = self.velocities[l - i];
-				if (v.x || v.y)
-					do {
-						self.velocity.x += v.x;
-						self.velocity.y += v.y;
-						v = self.velocities[l - (++i)];
-					} while (v && i <= 10)
-
-				self.velocity.x /= (l - i);
-				self.velocity.y /= (l - i);
-			}
 		});
 
-		on(elem, 'dragend', function (event) {
-			if (!/safari/i.test(window.navigator.userAgent) || /chrome/i.test(window.navigator.userAgent))
-				return;
-			var zone = document.getElementsByTagName('main')[0].getBoundingClientRect();
-			console.log(window.navigator.userAgent);
-			console.log(zone, event.clientX, event.clientY);
+		on(elem, 'dragstart', function (event) {
+			self.now = Date.now();
+			self.x = event.clientX;
+			self.y = event.clientY;
+			if (event.preventDefault)
+				event.preventDefault();
+			else
+				event.returnValue = false;
+		});
+
+		on(elem, 'mouseup', function (event) {
+			off(document, 'mousemove', self.handler);
+			if (self.velocities) {
+				var last = self.velocities[self.velocities.length - 1];
+				if (Math.abs(last.x) < ZERO && Math.abs(last.y) < ZERO)
+					self.velocity = {x: 0, y: 0};
+				else
+					self.velocity = self.velocities.reduce(function (a, b) {return {x: a.x + b.x, y: a.y + b.y};});
+			}
+			
+			delete self.velocities;
+			this.style.left = '';
+			this.style.top = '';
+			this.style.margin = '';
+			this.style.position = '';
+			this.style.zIndex = '';
+			self.x = event.clientX;
+			self.y = event.clientY;
+			if (inTarget(self.target, {x: event.clientX, y: event.clientY}))
+				self.target.dispatchEvent(new CustomEvent('drop', {detail: self}));
 		});
 	}
 
@@ -183,7 +221,7 @@
 					y: (coords.top + coords.bottom) / 2 - this.parent.top
 				};
 			},
-			set: function (coords) {
+			set: function (coords) {console.log(this.radius);
 				this.e.style.left = [coords.x - this.radius, 'px'].join('');
 				this.e.style.top = [coords.y - this.radius, 'px'].join('');
 			},
@@ -238,14 +276,6 @@
 
 		var self = this;
 
-		on(this.e, 'dragover', function (event) {
-			if (event.preventDefault)
-				event.preventDefault()
-			else
-				event.returnValue = true;
-			event.dataTransfer.dropEffect = 'move';
-		});
-
 		on(this.e, 'drop', function (event) {
 			if (event.preventDefault)
 				event.preventDefault()
@@ -253,19 +283,21 @@
 				event.returnValue = true;
 
 			for (var i = 0, l = self.dropped.length; i < l; ++i)
-				self.dropped[i].call(self, self.store.dropped, {x: event.clientX, y: event.clientY});
+				self.dropped[i].call(self, event.detail);
 		});
 	}
 
 	Object.defineProperties(Space.prototype, {
 		addPlanet: {
 			value: function (obj, coords) {
-				var p = new Planet(obj.e.src).bind(this)
+				var p = new Planet(obj.e.src).bind(this),
+				    self = this;
 				this.e.appendChild(p.e);
-				p.center = {x: coords.x, y: coords.y};
-				p.setVelocity(obj.velocity);
-				this.planets.push(p);
-
+				p.e.onload = function (event) {
+					p.center = {x: coords.x, y: coords.y};
+					p.setVelocity(obj.velocity);
+					self.planets.push(p);
+				}
 				return this;
 			},
 			writable: false,
@@ -301,10 +333,10 @@
 	});
 
 	window.onload = function () {
-		var space = new Space().ondropped(function(ball, coords) {
+		var space = new Space().ondropped(function(ball) {
 			if (!this.store.reload)
 				ball.hide();
-			return this.addPlanet(ball, {x: coords.x - this.left, y: coords.y - this.top});
+			return this.addPlanet(ball, {x: ball.x - this.left, y: ball.y - this.top});
 		});
 		console.log(space);
 	}
